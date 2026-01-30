556c9a0400de2adf71a6289f065215081bb1ac70
Shin-youngjae <a01037497436@gmail.com>
2026-01-30 19:09:12 +0900

    [ADD] : 성별 열거형 클래스

---
+++ backend/src/main/java/com/potatoes/backend/domain/Gender.java
@@ -0,0 +1,22 @@
+package com.potatoes.backend.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+    MALE("남자"),
+    FEMALE("여자");
+
+    private final String korean;
+
+    public static Gender fromKorean(String koreanGender) {
+        for (Gender gender : Gender.values()) {
+            if (gender.korean.equals(koreanGender)) {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 성별 입력입니다: " + koreanGender);
+    }
+}
